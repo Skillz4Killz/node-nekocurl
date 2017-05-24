6049b63b38955ec4271b84035de2ceceab8e7a48
Charlotte Dunois <github@charuru.moe>
2017-05-24 23:03:14 +0200

    pre-v0.2.6+1

--- src/nekocurl.js
+++ src/nekocurl.js
@@ -232,7 +232,7 @@ class Nekocurl {
             try {
                 response.body = JSON.parse(response.body);
             } catch(err) {
-                
+                /* continue regardless of error */
             }
         }
 
@@ -326,7 +326,7 @@ Nekocurl.isValidURL = (url) => {
     return /^(?:(?:https?):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(url);
 };
 
-const drivers = fs.readdirSync(__dirname+'/drivers/');
+const drivers = fs.readdirSync(path.join(__dirname, 'drivers'));
 for(let drivername of drivers) {
     if(drivername.endsWith('.driver.js')) {
         try {
