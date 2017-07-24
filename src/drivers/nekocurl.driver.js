dcf7a4b91a186884127b792331ef7d6a25a3687f
Charlotte Dunois <github@charuru.moe>
2017-07-24 21:33:57 +0200

    Fix?

--- src/drivers/nekocurl.driver.js
+++ src/drivers/nekocurl.driver.js
@@ -104,17 +104,6 @@ function applyOptionsToRequest(options) {
     return undefined;
 }
 
-function exportResObject(request, response, body, text) {
-    return {
-        request: request,
-        body: makeBody(response, body, text),
-        text: text,
-        headers: response.headers,
-        status: response.statusCode,
-        statusText: (response.statusText || http.STATUS_CODES[response.statusCode])
-    };
-}
-
 function driverNekocurl(options, driverOptions) {
     applyOptionsToRequest(options);
     
@@ -155,7 +144,14 @@ function driverNekocurl(options, driverOptions) {
                     return;
                 }
                 
-                const res = exportResObject(request, response, body, text);
+                const res = {
+                    request: request,
+                    body: makeBody(response, body, text),
+                    text: text,
+                    headers: response.headers,
+                    status: response.statusCode,
+                    statusText: (response.statusText || http.STATUS_CODES[response.statusCode])
+                };
                 
                 if(response.statusCode >= 200 && response.statusCode < 300) {
                     return resolve(res);
